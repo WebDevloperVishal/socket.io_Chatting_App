db959ec8deb587017b4d47deb119d799c63d5cbd
WebDevloperVisha <vishalsanam2024@gmail.com>
2025-09-26 16:17:01 +0530

    commit

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const express = require('express');
-const {  createServer } = require("node:http");
+const { createServer } = require("node:http");
 
 const app = express()
 
 app.use(express.json())
-
 
 server.listen(3000, () => {
  console.log(`Server is runnig on port http://localhost:3000`)
