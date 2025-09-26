668f8ab849eb1fcaaf7e747e239514d6044b3ca1
WebDevloperVisha <vishalsanam2024@gmail.com>
2025-09-26 17:15:25 +0530

    commit

--- index.js
+++ index.js
@@ -1,61 +1,30 @@
-// const express = require('express');
-// const { createServer } = require("node:http");
-// const { Server } = require('socket.io');
-
-// const app = express()
-// const server = createServer(app);
-// const io = new Server(server)
-
-// app.use(express.static('public'));
-
-// app.get('/', (req, res) => {
-//     return res.sendFile('index.html')
-// })
-
-// io.on("connection", (socket) => {
-//     console.log('A User Connected : '+ socket.id);
-
-//     socket.on("message", (msg)=>{
-//         console.log(msg);
-//         io.emit(msg);
-//     })
-
-//    socket.on('disconnect', () => {
-//     console.log('User Disconnected')
-//   })
-// })
-
-// server.listen(3000, () => {
-//     console.log(`Server is runnig on port http://localhost:3000`)
-// })
-
-const express = require('express')
-const { createServer } = require('node:http')
-const { Server } = require('socket.io')
+const express = require('express');
+const { createServer } = require("node:http");
+const { Server } = require('socket.io');
 
 const app = express()
-const server = createServer(app)
+const server = createServer(app);
 const io = new Server(server)
 
-app.use(express.static('public'))
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  return res.sendFile('index.html')
+    return res.sendFile('index.html')
 })
 
-io.on('connection', (socket) => {
-  console.log('A User Connected : ' + socket.id)
+io.on("connection", (socket) => {
+    console.log('A User Connected : ' + socket.id);
 
-  socket.on('message', (msg) => {
-    console.log(msg)
-    io.emit('message', msg)
-  })
+    socket.on("message", (msg) => {
+        console.log(msg);
+        io.emit('message', msg);
+    })
 
-   socket.on('disconnect', () => {
-    console.log('User Disconnected')
-  })
+    socket.on('disconnect', () => {
+        console.log('User Disconnected')
+    })
 })
 
 server.listen(3000, () => {
-  console.log('server running at 3000 Port')
+    console.log(`Server is runnig on port http://localhost:3000`)
 })
