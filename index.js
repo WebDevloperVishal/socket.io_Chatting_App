43317a08b29bd869b092cc41cc5d4719c128c0de
WebDevloperVisha <vishalsanam2024@gmail.com>
2025-09-26 16:29:26 +0530

    commit

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ const app = express()
 const server = createServer(app);
 const io = new Server(server)
 
+app.get('/', (req,res) =>{
+    return res.sendFile('index.html')
+})
+
 server.listen(3000, () => {
  console.log(`Server is runnig on port http://localhost:3000`)
 })
