81780e38901e1dd7e2fb80e2d873224fd79a898b
WebDevloperVisha <vishalsanam2024@gmail.com>
2025-09-26 16:27:30 +0530

    commit

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const { server } = require('socket.io');
 const app = express()
 
 const server = createServer(app);
-const io = new Server()
+const io = new Server(server)
 
 server.listen(3000, () => {
  console.log(`Server is runnig on port http://localhost:3000`)
